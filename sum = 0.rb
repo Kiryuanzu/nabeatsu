10a122e52fff697fd3bfc141fb1028783c7b4e72
桐生あんず <anzu.nagato.1005@gmail.com>
2017-04-04 00:43:03 +0900

    Update sum = 0.rb

--- sum = 0.rb
+++ sum = 0.rb
@@ -1,15 +1,16 @@
 #times do でのFizzbuzz
-i = 1
- 100.times do
+  i = 1
+  100.times do
 
-   if i % 15 == 0
+    if i % 15 == 0
     puts i.to_s + "は…fizzbuzz!!"
-   elsif i % 5 == 0
+    elsif i % 5 == 0
     puts i.to_s + "は…fizz!!"
-   elsif i % 3 == 0
+    elsif i % 3 == 0
     puts i.to_s + "は…buzz!!"
-   else
+    else
     puts  i
-   end
- i = i + 1
- end
+    end
+   
+    i = i + 1
+  end
