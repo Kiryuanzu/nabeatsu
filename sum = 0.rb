0b729a5e24c643d230a64cf16428855613e6a535
桐生あんず <anzu.nagato.1005@gmail.com>
2017-04-04 00:35:04 +0900

    Add files via upload

---
+++ sum = 0.rb
@@ -0,0 +1,15 @@
+#times do でのFizzbuzz
+i = 1
+ 100.times do
+
+   if i % 15 == 0
+    puts i.to_s + "は…fizzbuzz!!"
+   elsif i % 5 == 0
+    puts i.to_s + "は…fizz!!"
+   elsif i % 3 == 0
+    puts i.to_s + "は…buzz!!"
+   else
+    puts  i
+   end
+ i = i + 1
+ end
